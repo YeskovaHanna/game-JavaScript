b4f7161384c0dfda8c971346d045d333526daf59
Hanna Yeskova <eskovaanna5@gmail.com>
2023-02-26 17:23:00 +0200

    Add JS cod

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+
+    document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = `kamień`;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+printMessage('Zagralem' + playerMove + '! Jesli Twoj ruch to nozhnicy, to wygrywasz!');
+
+
+
+
 
