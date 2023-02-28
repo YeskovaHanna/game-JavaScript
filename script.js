139c53e36459a4039aca7cc92674a16d84d15273
Hanna Yeskova <eskovaanna5@gmail.com>
2023-02-28 15:42:55 +0200

    jawaS add

--- script.js
+++ script.js
@@ -16,6 +16,31 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 let playerMove = 'papier';
 printMessage('Zagralem' + playerMove + '! Jesli Twoj ruch to nozhnicy, to wygrywasz!');
 
+let randomFraction = Math.random();
+
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+
 
 
 
