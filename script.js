ffd0b28367516dda470fd7559d97fda996dd2d33
Hanna Yeskova <eskovaanna5@gmail.com>
2023-03-21 12:42:06 +0200

    rock scissors paper

--- script.js
+++ script.js
@@ -1,44 +1,96 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
+window .addEventListener('load',function ()
+ {
 
-    document.getElementById('messages').appendChild(div);
-}
+    let countUser = document.querySelector('.count-user'),
+        countComp = document.querySelector('.count-comp'),
+        userField = document.querySelector('.user-field'),
+        compField = document.querySelector('.comp-field'),
+        sound = document.querySelector('.sound'),
+        play = document.querySelector('.play'),
+        fields= document.querySelectorAll('.field'),
+        userStep, compStep, countU = 0,countC = 0, bloked = false;
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let computerMove = `kamień`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+    function choiceUser(e) {
+        if (bloked) return;
+        let target = e.target;
+        if (target.classList.contains('field')){
+            userStep = target.dataset.field;
+            fields.forEach(item =>item.classList.remove('active','error'));
+            target.class.List.add('active');
+            choiceComp();
+        }   
+    }
 
-let playerMove = 'papier';
-printMessage('Zagralem' + playerMove + '! Jesli Twoj ruch to nozhnicy, to wygrywasz!');
+    function choiceComp(){
+        bloked = true;
+        let rand = Math.floor(Math.random()*3);
+        compField.classList.add('blink');
+        let compField = compField.querySelectorAll('.field');
+    
+        setTimeout(() => {
+            compField.classList.remove('blink');
+            compStep = compFields[rand].dataset.field;
+            compFields[rand].classList.add ('active');
+            ServiceWorkerContainer();
+        },3000);
+    }
 
-let randomFraction = Math.random();
+    function winner(){
+        blocked = false;
+        let comb = userStep + compStep;
+        switch (comb){
+            case 'rr':
+            case 'ss':
+            case 'pp':
+                res.innerText = 'Ничья!';
+                sound.setAttribute('src','audio/draw.mp3');
+                sound.play();
+                break;
+            
+            case 'rs':
+            case 'sp':
+            case 'pr':
+                res.innerText = 'Победили вы!';
+                sound.setAttribute('src','audio/win.mp3');
+                sound.play();
+                countU++;
+                countUser.innerText = countU;
+                compField.querySelector('[data-field = '+compStep+']').classList.add('error');
+                break;
 
-printMessage('Wylosowany ułamek to: ' + randomFraction);
+            case 'sr':
+            case 'ps':
+            case 'rp':
+                res.innerText = 'Победил компьютер!';
+                sound.setAttribute('src','audio/loss.mp3');
+                sound.play();
+                countC++;
+                countComp.innerText = countC;
+                userField.querySelector('[data-field = '+userStep+']').classList.add('error');
+                break;
+        }
+    }
 
-let calculation = randomFraction * 3 + 1;
+   function playGame() {
+    countU = countC = 0;
+    res.innerText = 'Сделай выбор';
+    countUser.innerText = '0';
+    countComp.innerText = '0';
+    fields.forEach(item =>item.classList.remove('active','error'));
+   } 
 
-printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+   play.addEventListener('click', playGame);
+   userField.addEventListener('click', choiceUser);
 
-let roundNumber = Math.floor(calculation);
+ })
 
-printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+                        
+                
+        
+    
 
-let computerMove = 'nieznany ruch';
-
-if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-
-printMessage('Mój ruch to: ' + computerMove);
 
 
 
